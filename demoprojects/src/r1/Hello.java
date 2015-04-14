cd3f963472ac57c3fdd471c8047efff973780184
Shashankk <Shashankk@Shashank>
2015-04-14 00:49:39 -0700

    first commit

---
+++ demoprojects/src/r1/Hello.java
@@ -0,0 +1,18 @@
+package r1;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+	
+
+}
